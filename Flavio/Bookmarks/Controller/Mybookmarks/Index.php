03a723e39c956e2e755d09406f8ef8babb3342d8
flavioperezcarreto <flavioperezcarreto@gmail.com>
2023-10-24 17:18:24 -0600

    Updated page title base on cmsBlock title.

--- Flavio/Bookmarks/Controller/Mybookmarks/Index.php
+++ Flavio/Bookmarks/Controller/Mybookmarks/Index.php
@@ -5,21 +5,23 @@ namespace Flavio\Bookmarks\Controller\Mybookmarks;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Framework\App\Action\Context;
+use Magento\Cms\Api\BlockRepositoryInterface;
 
 class Index implements HttpGetActionInterface
 {
+    const CMS_BLOCK_IDENTIFIER = 'MyBookmarksWelcomeMessage';
+
     public function __construct(
         private readonly PageFactory $pageFactory,
-        //Context $context,
-    ){
-        //parent::__construct($context);
-    }
+        private readonly BlockRepositoryInterface $blockRepository,
+    ){}
 
     public function execute(): Page
     {
         $page = $this->pageFactory->create();
-        $page->getConfig()->getTitle()->set("My Bookmarks");
+        $block = $this->blockRepository->getById(self::CMS_BLOCK_IDENTIFIER);
+        $title = $block->getTitle();
+        $page->getConfig()->getTitle()->set($title ?? 'My Bookmarks');
         return $page;
     }
 }
