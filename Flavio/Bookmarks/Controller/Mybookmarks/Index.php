993f344a8ac6074c5834435bfc853be8be56ac78
flavioperezcarreto <flavioperezcarreto@gmail.com>
2023-11-02 17:19:21 -0600

    - validate if Bookmark parameter if is on return My Bookmarks page otherwise redirect to About Us.

--- Flavio/Bookmarks/Controller/Mybookmarks/Index.php
+++ Flavio/Bookmarks/Controller/Mybookmarks/Index.php
@@ -3,20 +3,34 @@
 namespace Flavio\Bookmarks\Controller\Mybookmarks;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\View\Result\Page;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\View\Result\PageFactory;
+
+//use Magento\Backend\Model\View\Result\Redirect;
 
 class Index implements HttpGetActionInterface
 {
     public function __construct(
         private readonly PageFactory $pageFactory,
+        private readonly ResultFactory $resultFactory,
+        private readonly ScopeConfigInterface $scopeConfig,
     ){}
 
-    public function execute(): Page
+    public function execute()
     {
         $page = $this->pageFactory->create();
         $page->getConfig()->getTitle()->set('My Bookmarks');
-        return $page;
+
+        $value = $this->scopeConfig->getValue('bookmarks_for_customers/general/enable');
+
+        $isBookmarkParameterOn = ((isset($value) && $value == "1"));
+
+        if ($isBookmarkParameterOn) {
+            return $page;
+        }
+        $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $redirect->setPath('about-us');
     }
 }
 
