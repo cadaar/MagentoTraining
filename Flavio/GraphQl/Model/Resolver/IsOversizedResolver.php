33164810a17364e0d6a1af952ed257f10a715a55
flavioperezcarreto <flavioperezcarreto@gmail.com>
2023-12-14 06:19:34 -0600

    - Initial version.

---
+++ Flavio/GraphQl/Model/Resolver/IsOversizedResolver.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Flavio\GraphQl\Model\Resolver;
+
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class IsOversizedResolver implements ResolverInterface
+{
+
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ):bool
+    {
+        $product = $value['model'];
+
+        return $product->getWeight() >= 50;
+    }
+}
